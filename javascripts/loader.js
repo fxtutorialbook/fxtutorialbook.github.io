c00be2231515dc5ad6e456bf82ffba751d658e2c
kenokabe <kenokabe@gmail.com>
2014-11-14 20:10:46 +0900

    commit

--- javascripts/loader.js
+++ javascripts/loader.js
@@ -4,20 +4,9 @@
 /* global window,document, $,alert,history, imagesLoaded */
 'use strict';
 
-var log = function(msg)
-{
-  console.log('CORE:', msg);
-};
-log('init5');
-
-//var _ = require('spacetime').lazy();
-//var __ = require('spacetime').timeline();
-
- 
 
 $(document)
   .ready(function()
   {
-    alert();
     $('#content').load('./fxtutorialbook.html');
   });
